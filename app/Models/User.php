716a17c1eeb4cd8ab98d22cb1085a87cd4b7f20a
hermancahyono <cahyonoherman93@gmail.com>
2023-05-19 12:20:37 +0700

    add Trait HasScope to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
+use App\Traits\HasScope;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -13,73 +14,72 @@ use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+  use HasApiTokens, HasFactory, Notifiable, HasRoles, HasScope;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-        'username',
-        'avatar',
-        'github',
-        'instagram',
-        'about'
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $fillable = [
+    'name',
+    'email',
+    'password',
+    'username',
+    'avatar',
+    'github',
+    'instagram',
+    'about'
+  ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-        'two_factor_secret',
-        'two_factor_recovery_codes'
-    ];
+  /**
+   * The attributes that should be hidden for serialization.
+   *
+   * @var array<int, string>
+   */
+  protected $hidden = [
+    'password',
+    'remember_token',
+    'two_factor_secret',
+    'two_factor_recovery_codes'
+  ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+  /**
+   * The attributes that should be cast.
+   *
+   * @var array<string, string>
+   */
+  protected $casts = [
+    'email_verified_at' => 'datetime',
+  ];
 
-    // add accesor for avatar
+  // add accesor for avatar
 
-    public function avatar(): Attribute
-    {
-        return Attribute::make(
-            get: fn ($avatar) => $avatar != '' ? asset('storage/avatars/' . $this->$avatar) : 'https://ui-avatars.com/api/?name=' . str_replace(' ', '+', $this->name) . '&background=4e73df&color=ffffff&size=100',
-        );
-    }
-    
-    // Relationships
-    public function courses()
-    {
-        return $this->hasMany(Course::class);
-    }
+  public function avatar(): Attribute
+  {
+    return Attribute::make(
+      get: fn ($avatar) => $avatar != '' ? asset('storage/avatars/' . $this->$avatar) : 'https://ui-avatars.com/api/?name=' . str_replace(' ', '+', $this->name) . '&background=4e73df&color=ffffff&size=100',
+    );
+  }
 
-    public function carts()
-    {
-        return $this->hasMany(Cart::class);
-    }
-    
-    public function reviews()
-    {
-        return $this->hasMany(Review::class);
-    }
+  // Relationships
+  public function courses()
+  {
+    return $this->hasMany(Course::class);
+  }
 
-    public function showcases()
-    {
-        return $this->hasMany(Showcase::class);
-    }
+  public function carts()
+  {
+    return $this->hasMany(Cart::class);
+  }
 
+  public function reviews()
+  {
+    return $this->hasMany(Review::class);
+  }
+
+  public function showcases()
+  {
+    return $this->hasMany(Showcase::class);
+  }
 }
