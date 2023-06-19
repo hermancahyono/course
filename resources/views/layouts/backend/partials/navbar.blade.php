abfbc470476b8a690527c33c6a7a3da8ea0199c1
hermancahyono <cahyonoherman93@gmail.com>
2023-06-19 23:15:03 +0700

    notification on navbar

--- resources/views/layouts/backend/partials/navbar.blade.php
+++ resources/views/layouts/backend/partials/navbar.blade.php
@@ -13,14 +13,33 @@
             <a class="nav-link" data-toggle="dropdown" href="#">
                 <i class="far fa-bell"></i>
                 <span class="badge badge-warning navbar-badge">
-                    10
+                    {{ Auth::User()->unreadNotifications->count() }}
                 </span>
             </a>
             <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
                 <span class="dropdown-item dropdown-header">
-                    10 Notifications
+                    {{ Auth::user()->unreadNotifications->count() }} Notifications
                 </span>
                 <div class="dropdown-divider"></div>
+                @foreach (Auth::user()->notifications as $notification)
+                    @if ($notification->read_at == null)
+                        <form action="{{ route('admin.markNotification', $notification->id) }}" method="POST">
+                            @csrf
+                            <button type="submit" class="dropdown-item">
+                                <i class="fas fa-info-circle mr-2"></i>{{ $notification->data['message'] }}
+                                <span
+                                    class="float-right text-muted text-sm">{{ $notification->created_at->diffForHumans() }}</span>
+                            </button>
+                        </form>
+                    @endif
+                    @if ($loop->last)
+                        <div class="dropdown-divider"></div>
+                        <form action="{{ route('admin.markAllRead') }}" method="POST">
+                            @csrf
+                            <button type="submit" class="dropdown-item dropdown-footer">Mark as Read</button>
+                        </form>
+                    @endif
+                @endforeach
             </div>
         </li>
         <li class="nav-item">
