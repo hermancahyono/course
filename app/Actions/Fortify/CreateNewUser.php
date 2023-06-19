23fbdcb31f445116e77771db11181bf4a22362c5
hermancahyono <cahyonoherman93@gmail.com>
2023-06-19 23:19:21 +0700

    add app/Actions/Fortify/CreateNewUser.php

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -14,39 +14,43 @@ use Laravel\Fortify\Contracts\CreatesNewUsers;
 
 class CreateNewUser implements CreatesNewUsers
 {
-    use PasswordValidationRules;
+  use PasswordValidationRules;
 
-    /**
-     * Validate and create a newly registered user.
-     *
-     * @param  array  $input
-     * @return \App\Models\User
-     */
-    public function create(array $input)
-    {
-        Validator::make($input, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => [
-                'required',
-                'string',
-                'email',
-                'max:255',
-                Rule::unique(User::class),
-            ],
-            'password' => $this->passwordRules(),
-        ])->validate();
+  /**
+   * Validate and create a newly registered user.
+   *
+   * @param  array  $input
+   * @return \App\Models\User
+   */
+  public function create(array $input)
+  {
+    Validator::make($input, [
+      'name' => ['required', 'string', 'max:255'],
+      'email' => [
+        'required',
+        'string',
+        'email',
+        'max:255',
+        Rule::unique(User::class),
+      ],
+      'password' => $this->passwordRules(),
+    ])->validate();
 
-        $role = Role::where('name', 'member')->first();
+    $role = Role::where('name', 'member')->first();
 
-        $user = User::create([
-            'name' => $input['name'],
-            'username' => $input['name'] .'-'. Str::random(6),
-            'email' => $input['email'],
-            'password' => Hash::make($input['password']),
-        ]);
+    $user = User::create([
+      'name' => $input['name'],
+      'username' => $input['name'] . '-' . Str::random(6),
+      'email' => $input['email'],
+      'password' => Hash::make($input['password']),
+    ]);
 
-        $user->assignRole($role);
+    $user->assignRole($role);
 
-        return $user;
-    }
+    $admin = User::role('admin')->get();
+
+    Notification::send($admin, new NewMember($user));
+
+    return $user;
+  }
 }
