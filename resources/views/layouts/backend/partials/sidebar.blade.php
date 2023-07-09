8c0a8ae67b3915f2801dc9028674df7ab9dcba52
hermancahyono <cahyonoherman93@gmail.com>
2023-07-09 17:37:54 +0700

    edit sidebar for home route

--- resources/views/layouts/backend/partials/sidebar.blade.php
+++ resources/views/layouts/backend/partials/sidebar.blade.php
@@ -1,5 +1,5 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <a href="" class="brand-link">
+    <a href="{{ route('home') }}" class="brand-link">
         <div class="text-center">
             <span class="brand-text font-weight-bold">LaraDev</span>
         </div>
